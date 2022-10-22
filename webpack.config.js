fb372f232cfcf7fb244fc6b0b1fbbd5375a80878
Nate Foster <nate.s.foster@gmail.com>
2022-10-22 11:36:02 -1000

    Build out directories and set up webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, './client/src');
+const DIST_DIR = path.join(__dirname, './client/dist');
+
+module.exports = {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
